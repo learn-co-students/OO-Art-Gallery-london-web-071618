c54ce484a8f57ac46c6f84271f71266e3471c2b5
ritz-w <ritz.wu@outlook.com>
2018-07-27 17:20:44 +0100

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,11 +1,35 @@
 class Gallery
 
-  attr_reader :name, :city
+  attr_reader :name, :cityr
+  @@all = []
+    def initialize(name, city)
+      @name = name
+      @city = city
+      @@all << self
+    end
 
-def initialize(name, city)
-  @name = name
-  @city = city
-end
+    def self.all
+      @@all
+    end
+
+    def self.all_cities
+      self.all.map {|x| x.city}.uniq
+    end
+
+    def self.artists_from(gallery)
+      select_painting = Painting.all.select {|x| x.gallery == gallery}
+      select_painting.map {|x| x.artist}
+    end
+
+    def self.artists_names_from(gallery)
+      artists_list = self.artists_from(gallery)
+      artists_list.map {|x| x.name}
+    end
+
+    def years_experience_at
+      gallery_artists = Gallery.artists_from(self)
+      gallery_artists.map {|x| x.years_active}.inject(:+)
+    end
 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,5 +3,15 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+pollock = Artist.new("Jackson Pollock", 30)
+kooning = Artist.new("De kooning", 20)
+arbus = Artist.new("Diane Arbus", 27)
+white_cube = Gallery.new("white cube", "London")
+tate = Gallery.new("Tate", "London")
+pompidou = Gallery.new("Pompidou", "Paris")
+
+pollock_27 = Painting.new("27", "ab ex", pollock, tate)
+kooning_100 = Painting.new("100", "ab ex", kooning, white_cube)
+pollock_80 = Painting.new("80", "ab ex", pollock, pompidou)
 
 binding.pry
