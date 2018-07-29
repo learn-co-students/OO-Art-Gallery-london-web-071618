44a54c427a66e9be36f358e2ee3cc3ff21072100
github username <github email address>
2018-07-29 13:23:03 +0100

    done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,35 @@
 class Artist
 
-  attr_reader :name
+  attr_accessor :name, :years_active
 
-  def initialize(name, years_active)
-    @name = name
-    @years_active = years_active
-  end
+  @@all = []
 
+      def initialize(name, years_active)
+          @name = name
+          @years_active = years_active
+          @@all << self
+      end
+
+      def self.all
+          @@all
+      end
+
+      def paintings
+          Painting.all.select {|painting| painting.artist == self}
+      end
+
+      def galleries
+          paintings.map {|painting| painting.gallery}
+      end
+
+      def cities
+          galleries.map {|gallery| gallery.city}
+      end
+
+      def self.avg_experience
+          total_experience = 0
+          Artist.all.each {|artist| total_experience += artist.years_active}
+          total_experience/ (Artist.all.size)
+      end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,21 @@ def reload
   load 'config/environment.rb'
 end
 
+
+sarah = Artist.new("sarah", 6)
+maduri = Artist.new("maduri", 2)
+ritz = Artist.new("ritz", 8)
+
+gallery1 = Gallery.new("gallery1", "London")
+gallery2 = Gallery.new("gallery2", "NY")
+gallery3 = Gallery.new("gallery3", "Berlin")
+gallery4 = Gallery.new("gallery4", "Paris")
+gallery4 = Gallery.new("gallery5", "Paris")
+
+
+painting1 = Painting.new("painting1", "abstract", sarah, gallery1)
+painting2 = Painting.new("painting2", "impressionist", ritz, gallery2)
+painting3 = Painting.new("painting3", "finger painting", maduri, gallery2)
+painting4 = Painting.new("painting4", "abstract", ritz, gallery4)
+
 binding.pry
