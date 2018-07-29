306bdb26f6f600faf8eece18261cfe7da3c5664e
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-07-29 18:14:01 +0100

    Jo's solution

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,34 @@
 class Artist
 
-  attr_reader :name
+@@all = []
+
+  attr_reader :name, :years_active
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    self.paintings.collect {|painting| painting.gallery}
+  end
+
+  def cities_with_artists_paintings
+    self.galleries.collect {|gallery| gallery.city}.uniq
+  end
+
+  def self._years_experience
+    yrs_active_all_artists = Artist.all.collect{|artist| artist.years_active.to_f}
+    (yrs_active_all_artists.inject{|x, y| x + y } / yrs_active_all_artists.count).round(2)
+  end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,10 +1,21 @@
 class Painting
+@@all = []
 
-  attr_reader :title, :style
+  attr_reader :title, :style, :artist, :gallery
 
-  def initialize(title, style)
+  def initialize(title, style, artist, gallery)
     @title = title
     @style = style
+    @artist = artist
+    @gallery = gallery
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.painting_styles
+    @@all.collect {|painting| painting.style}.uniq
+  end
 end
